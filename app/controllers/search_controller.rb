5b9af8848b38d811a6cf4144a290afededbdcf78
higakijin <hm385.chejptks@gmail.com>
2021-08-28 16:52:35 +0900

    ⚡️ make links and change forms

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,24 @@
 class SearchController < ApplicationController
   def search
-    year = params[:year]
-    season = params[:season]
-    if year.present? && season.present?
-      uri = URI.parse("https://api.moemoe.tokyo/anime/v1/master/#{year}/#{season}")
+    @year = params[:year]
+    season_num = params[:season]
+    if @year.present? && season_num.present?
+      uri = URI.parse("https://api.moemoe.tokyo/anime/v1/master/#{@year}/#{season_num}")
       response = Net::HTTP.get(uri)
       @result = JSON.parse(response)
-    else
+
+      if season_num == "1"
+        season = "冬（1〜3月）"
+      elsif season_num == "2"
+        season = "春（4〜6月）"
+      elsif season_num == "3"
+        season = "夏（7〜9月）"
+      elsif season_num == "4"
+        season = "秋（10〜12月）"
+      end
+
+      @season = season
+    # else
     end
   end
 end
