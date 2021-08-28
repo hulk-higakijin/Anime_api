9901696f0260b4f5b60a2d4fcc0ae087cc5e0d4e
higakijin <hm385.chejptks@gmail.com>
2021-08-28 12:42:02 +0900

    🐛chang view if  I post empty data

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,10 +2,11 @@ class SearchController < ApplicationController
   def search
     year = params[:year]
     season = params[:season]
-    if year && season
+    if year.present? && season.present?
       uri = URI.parse("https://api.moemoe.tokyo/anime/v1/master/#{year}/#{season}")
       response = Net::HTTP.get(uri)
       @result = JSON.parse(response)
+    else
     end
   end
 end
