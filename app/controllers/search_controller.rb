5fcfc7345c46f8d774037064361df2e5b7475904
higakijin <hm385.chejptks@gmail.com>
2021-08-28 19:54:27 +0900

    🐛 change condition on form

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,11 +2,10 @@ class SearchController < ApplicationController
   def search
     @year = params[:year]
     season_num = params[:season]
-    if @year.present? && season_num.present?
+    if @year.to_i <= 2021 && @year.to_i >= 2014 && season_num.to_i >= 1 && season_num.to_i <= 4
       uri = URI.parse("https://api.moemoe.tokyo/anime/v1/master/#{@year}/#{season_num}")
       response = Net::HTTP.get(uri)
       @result = JSON.parse(response)
-
       if season_num == "1"
         season = "冬（1〜3月）"
       elsif season_num == "2"
@@ -16,9 +15,8 @@ class SearchController < ApplicationController
       elsif season_num == "4"
         season = "秋（10〜12月）"
       end
-
       @season = season
-    # else
+      
     end
   end
 end
